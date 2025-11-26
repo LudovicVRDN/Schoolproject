4381c6e92d14147cbb8d40afd7093aee5ac1be6e
LudovicVRDN <vourdon@live.fr>
2025-11-26 09:10:05 +0100

    ADD JS

---
+++ KATA1000/script/script.js
@@ -0,0 +1,9 @@
+//ajout du boutton//
+
+let popup_button = document.querySelector(".popup_button");
+
+//Ajout de la fonction toggleup//
+function togglePopup() {
+    let contact = document.querySelector(".contact_section");
+    contact.classList.toggle("open");
+}
